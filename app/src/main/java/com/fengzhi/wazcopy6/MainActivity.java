dbc390b894ea3a4bd1770c2036eca0bb818cab83
smilemolj <smilemolj@gmail.com>
2019-06-24 15:58:06 +0800

    init

---
+++ app/src/main/java/com/fengzhi/wazcopy6/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fengzhi.wazcopy6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
